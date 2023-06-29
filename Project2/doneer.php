407238f9fcaee77b3181c0c3a62d80c84c61a31a
NoahKlaassen1 <ps246628@summacollege.nl>
2023-06-29 09:52:55 +0200

    Doneer pagina
    Ik heb een nieuwe css file gemaakt voor de doneer pagina en daar heb ik dan de style changes in gemaakt.

--- Project2/doneer.php
+++ Project2/doneer.php
@@ -4,24 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <title>Home Pagina</title>
+    <link rel="stylesheet" type="text/css" href="doneer.css">
+    <title>Doneer</title>
 </head>
 <body>
     <header>
     <?php include 'header.php'; ?>
     </header>
     
-    <div style="text-align: center; margin-bottom: -150px; margin-top: 50px;">
-        <h2 style="color: #05da10; font-size: 50px;">Doneer</h2>
+    <div class="centered">
+        <h2 class="green-heading">Doneer</h2>
     </div>
-    <form id="myForm" action="form-doneer.php" method="POST" style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
-        <input type="text" name="name" placeholder="Naam" required style="margin-bottom: 30px; width: 550px; height: 50px;">
-        <input type="email" name="email" placeholder="E-mail" required style="margin-bottom: 30px; width: 550px; height: 50px;">
-        <input type="text" name="Woonplaats" placeholder="Woonplaats" required style="margin-bottom: 30px; width: 550px; height: 50px;">
-        <input type="number" name="Leeftijd" placeholder="Leeftijd" required style="margin-bottom: 30px; width: 550px; height: 50px;">
-        <input type="number" name="Bedrag" placeholder="Bedrag" required style="margin-bottom: 30px; width: 550px; height: 50px;">
-        <button type="submit" style="background-color: #05da10; color: #ffffff; padding: 10px 20px; border: none; border-radius: 5px; font-size: 16px; cursor: pointer; transition: background-color 0.3s ease;">Verzenden</button>
+    <form id="myForm" action="form-doneer.php" method="POST" class="form-container">
+        <input type="text" name="name" placeholder="Naam" required class="input-field">
+        <input type="email" name="email" placeholder="E-mail" required class="input-field">
+        <input type="text" name="Woonplaats" placeholder="Woonplaats" required class="input-field">
+        <input type="number" name="Leeftijd" placeholder="Leeftijd" required class="input-field">
+        <input type="number" name="Bedrag" placeholder="Bedrag" required class="input-field">
+        <button type="submit" class="submit-button">Verzenden</button>
     </form>
     
     
