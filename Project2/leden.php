5467b4877406595356dd4043ccb1ac9ef3b93b92
NoahKlaassen1 <ps246628@summacollege.nl>
2023-07-03 09:58:14 +0200

    adf
    sdfa

--- Project2/leden.php
+++ Project2/leden.php
@@ -14,72 +14,72 @@
     <h1 class="title">Leden</h1>
     <section class="container">
         <!-- Card 1 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-1"></div>
             <h2>Pieter Heerma</h2>
         </div>
         <!-- Card 2 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-2"></div>
             <h2>Anne Kuik</h2>
         </div>
         <!-- Card 3 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-3"></div>
             <h2>Inge van Dijk</h2>
         </div>
         <!-- Card 4 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-4"></div>
             <h2>Lucille Werner</h2>
         </div>
         <!-- Card 5 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-5"></div>
             <h2>Derk Boswijk</h2>
         </div>
         <!-- Card 6 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-6"></div>
             <h2>Eline Vedder</h2>
         </div>
         <!-- Card 7 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-7"></div>
             <h2>Evertjan Slootweg</h2>
         </div>
         <!-- Card 8 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-8"></div>
             <h2>Harmen Krul</h2>
         </div>
         <!-- Card 9 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-9"></div>
             <h2>Henri Bontenbal</h2>
         </div>
         <!-- Card 10 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-10"></div>
             <h2>Hilde Mulder</h2>
         </div>
         <!-- Card 11 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-11"></div>
             <h2>Joba van den Berg</h2>
         </div>
         <!-- Card 12 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-12"></div>
             <h2>Mustafa Amhaouch</h2>
         </div>
         <!-- Card 13 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-13"></div>
             <h2>Bart van den Brink</h2>
         </div>
         <!-- Card 14 -->
-        <div class="card">
+        <div class="cards">
             <div class="card-image card-14"></div>
             <h2>Rene Peters</h2>
         </div>
