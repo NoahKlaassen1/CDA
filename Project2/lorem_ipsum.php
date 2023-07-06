110e4dad4db6b282267c90e4d57f1755ebdf1172
NoahKlaassen1 <ps246628@summacollege.nl>
2023-07-06 13:18:41 +0200

    ...

---
+++ Project2/lorem_ipsum.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/lorem_ipsum.css">
+    <title>Lorem Ipsum</title>
+</head>
+<body>
+<header>
+        <?php include 'header.php'; ?>
+    </header>
+<h1>Lorem Ipsum</h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac libero auctor, pulvinar erat ut, semper justo. Phasellus eu elit ultricies, facilisis leo ut, finibus nulla. Ut vulputate pellentesque nunc, at efficitur mauris viverra non. Suspendisse sit amet lacus eget orci tempus convallis. Donec at sagittis purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis fermentum mi quis sem tristique, sit amet blandit elit rhoncus. Nulla efficitur enim quis iaculis tristique. Suspendisse et massa vel nulla congue ullamcorper. Vestibulum congue bibendum augue, ut efficitur metus ullamcorper id. Donec facilisis elementum tellus, at aliquam felis ultrices sed.
+  </p>
+  
+</body>
+</html>
